80046b05b91bd105643ad4537ec66aeb18cf6ea9
Mamoon Tahir <mamoonbintahir@gmail.com>
2025-08-04 09:18:01 +0500

    initial commit

---
+++ javascript/practice/practice21.js
@@ -0,0 +1,22 @@
+// let userRole = 'plumber';
+
+// function checkAccess(featureName) {
+//     let hasAccess = false;
+
+//     if (userRole === "Admin") {
+//         hasAccess = true;
+//     } else if (userRole === "Viewer") {
+//         hasAccess = true;
+//     } else if (userRole === "Editor") {
+//         if (["Edit", "add", "remove"].includes(featureName)) {
+//             hasAccess = true;
+//         }
+//     }
+
+//     return hasAccess;
+// }
+
+// console.log(checkAccess("Edit"));
+
+
+
